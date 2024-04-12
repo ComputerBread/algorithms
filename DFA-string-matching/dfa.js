db09df1e0591519267ca8bbccabb06b1abe9d6f2
ComputerBread <contact.computerbread@gmail.com>
2024-04-12 02:55:21 +0200

    DFA string matching

--- DFA-string-matching/dfa.js
+++ DFA-string-matching/dfa.js
@@ -1,27 +1,55 @@
 function constructDFA(pattern) {
     const m = pattern.length;
-    const P = pattern.split('').map(char => char.charCodeAt(0) - 'a'.charCodeAt(0));
-    const alphabetLength = 4;
-    const dfa = new Array(m);
-    for (let i = 0; i < m; i++) {
-        dfa[i] = new Array(alphabetLength);
-    }
-    for (let i = 0; i < alphabetLength; i++) {
-        dfa[0][i] = 0;
-    }
-    dfa[0][P[0]] = 1;
-    let x = 0;
-    for ( let j = 1; j < pattern.length; j++) {
-        for (let i = 0; i < alphabetLength; i++) {
-            dfa[j][i] = dfa[x][i];
-        }
-        dfa[j][P[j]] = j + 1;
-        x = dfa[x][P[j]];
-    }
-    return dfa;
+    //const P = pattern.split('').map(char => char.charCodeAt(0) - 'a'.charCodeAt(0));
+    const P = pattern.split('').map(char => char.charCodeAt(0));
+    const alphabetLength = 128; // ASCII
 
+    // Create transition matrix (m+1 states, alphabet length letters)
+    const transitions = new Array(m+1);
+    for (let i = 0; i <= m; i++) {
+        transitions[i] = new Array(alphabetLength);
+    }
+    // initialize first row to 0. In state 0, all letters go to zero...
+    for (let i = 0; i < alphabetLength; i++) {
+        transitions[0][i] = 0;
+    }
+    // ...except first letter of pattern, which leads to state 1.
+    transitions[0][P[0]] = 1;
+
+    let x = 0; // state in which automaton is after reading P[1..q-1]
+
+    for ( let q = 1; q <= m; q++) {
+        for (let i = 0; i < alphabetLength; i++) {
+            transitions[q][i] = transitions[x][i];
+        }
+        transitions[q][P[q]] = q + 1;
+        x = transitions[x][P[q]];
+    }
+    return transitions;
 }
-function dfa_str_match(){}
+
+function dfa_str_match(pattern, text){
+    const n = text.length;
+    const m = pattern.length;
+    const occurences = [];
+    const dfa = constructDFA(pattern);
+    //const T = text.split('').map(char => char.charCodeAt(0) - 'a'.charCodeAt(0));
+    const T = text.split('').map(char => char.charCodeAt(0));
+    let state = 0;
+    for ( let i = 0; i < n; i++ ) {
+        state = dfa[state][T[i]];
+        if (state === m) {
+            occurences.push(i-m+1);
+        }
+    }
+    return occurences;
+}
 
 console.log(JSON.stringify(constructDFA("ababac")))
 console.log(JSON.stringify(constructDFA("abcabdab")))
+const text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaabbbbbbbbbbbbbbbbbbbabaaaaaababbbbbaba";
+console.log(dfa_str_match("aba",text))
+console.log(text.slice(27, 30))
+console.log(text.slice(55, 58))
+console.log(text.slice(62, 65))
+console.log(text.slice(70, 73))
