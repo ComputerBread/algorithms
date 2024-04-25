b19ac8faf6a11edd6127b638d703c99b6de52558
ComputerBread <contact.computerbread@gmail.com>
2024-04-25 17:56:36 +0200

    aho-corasick isn't sick anymore

--- Aho-Corasick/aho-corasick.js
+++ Aho-Corasick/aho-corasick.js
@@ -1,13 +1,16 @@
 const FAIL = -1;
-const alphabetLength = 128; // ASCII
+const alphabetLength = 5; // ASCII
 
 function addNewState(g) {
+    let m = g.length;
     g.push(new Array(alphabetLength));
-    for (let i = 0; i < alphabetLength; i++) g[0][i] = FAIL;
+    for (let i = 0; i < alphabetLength; i++) g[m][i] = FAIL;
 }
 
 function AhoCorasick() {
-    const keywords = ["hers", "his", "she"];
+    // const keywords = ["he", "she", "his", "hers"];
+    const keywords = ["ab", "dab", "aed", "abcd"];
+    // h -> a, e -> b, r -> c, s -> d, i -> e
     let newstate = 0;
 
     const g = [];
@@ -25,7 +28,7 @@ function AhoCorasick() {
     }
 
     console.table(g);
-    console.table(failureFunc(g, output));
+    console.log(JSON.stringify(failureFunc(g, output)));
     console.table(output);
 
     // because of newstate
@@ -33,14 +36,15 @@ function AhoCorasick() {
         const m = keyword.length;
         let state = 0;
         let j = 0;
-        while (g[state][c = keyword[j].charCodeAt(0)] != FAIL) {
+        let c;
+        while (j < m && g[state][c = keyword[j].charCodeAt(0)-97] != FAIL) {
             // nah I am goign to jail for this, now i am real ninja programmar, ahah you got it, it's funny because
             state = g[state][c];
             j++;
         }
         for (let p = j; p < m; p++) {
             newstate++;
-            g[state][keyword[p].charCodeAt(0)];
+            g[state][keyword[p].charCodeAt(0)-97] = newstate;
             state = newstate;
             addNewState(g);
         }
@@ -58,6 +62,7 @@ function AhoCorasick() {
 function failureFunc(g, output) {
     const failure = new Array(g.length-1);
     const queue = [];
+
     g[0].forEach(s => {
        if (s != 0) {
         queue.push(s);
@@ -67,16 +72,26 @@ function failureFunc(g, output) {
 
     while (queue.length != 0) {
         let r = queue.shift();
-
+        let state;
 
         for (let a = 0; a < alphabetLength; a++) {
             let s;
             if ((s = g[r][a]) === FAIL) continue;
-            do {
-                state = failure[r];
-            } while(g[r][a] === FAIL);
+
+            queue.push(s);
+            state = failure[r];
+            while(g[state][a] === FAIL) {
+                state = failure[state];
+            }
+
             failure[s] = g[state][a];
-            output[s].add(output[failure[s]]);
+            console.log(s);
+            if (s === 5) debugger;
+            if (!output.has(s) && output.has(failure[s])) {
+                output.set(s,new Set());
+            }
+            if (output.has(failure[s]))
+                output.get(s).add(...output.get(failure[s]));
         }
 
         
